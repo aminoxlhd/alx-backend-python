f82a024da4d86db5fb258ab0ee5eaa00719c4782
aminoxlhd <aminoxlhd@gmail.com>
2024-05-02 14:45:23 +0100

    add 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" takes a string str1 and a string str2 and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ concat function """
+    return str1 + str2
